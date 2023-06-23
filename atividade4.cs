6f2bbb82fe06d816cd5da5dfa1e572e18f242a5c
CaioPQD <128498808+CaioPQD@users.noreply.github.com>
2023-06-23 00:08:17 -0300

    Update atividade4.cs

--- atividade4.cs
+++ atividade4.cs
@@ -11,20 +11,20 @@ namespace Lista1
             double x2 = 0;
             double y2 = 0;
 
-            Console.WriteLine("Digite um valor para X1: ");
+            Console.WriteLine("infrme um valor para X1: ");
             Console.Write("> ");
             x1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite um valor para Y1: ");
+            Console.WriteLine("Informe um valor para Y1: ");
             Console.Write("> ");
             y1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite um valor para X2: ");
+            Console.WriteLine("informe um valor para X2: ");
             Console.Write("> ");
             x2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite um valor para Y2: ");
+            Console.WriteLine("informe um valor para Y2: ");
             Console.Write("> ");
             y2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("A distancia entre os 2 pontos é: " + Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2)));
+            Console.WriteLine("A distancia entre os 2 pontos sera de: " + Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2)));
         }
     }
 }
