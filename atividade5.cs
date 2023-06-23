6d3dc89414f98bd1d5b663e6504042d07145acd1
CaioPQD <128498808+CaioPQD@users.noreply.github.com>
2023-06-23 00:09:26 -0300

    Update atividade5.cs

--- atividade5.cs
+++ atividade5.cs
@@ -11,17 +11,17 @@ namespace Lista1
             int numMeses = 0;
 
 
-            Console.WriteLine("Digite o valor da aplicaçao mensal: ");
-            Console.Write("> ");
+            Console.WriteLine("informe o valor da aplicaçao mensal: ");
+            Console.Write(">");
             aplicacaoMensal = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o valor da taxa: ");
-            Console.Write("> ");
+            Console.WriteLine("informe o valor da taxa: ");
+            Console.Write(">");
             taxa = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o numero de meses: ");
-            Console.Write("> ");
+            Console.WriteLine("informe o numero de meses: ");
+            Console.Write(">");
             numMeses = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Seu rendimento é: " + (aplicacaoMensal * (Math.Pow((1 + taxa),numMeses) - 1)/taxa));
+            Console.WriteLine("Seu rendimento sera de: " + (aplicacaoMensal * (Math.Pow((1 + taxa),numMeses) - 1)/taxa));
         }
     }
 }
