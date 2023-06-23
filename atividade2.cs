12b02c9ed98982d314d494e31553c270e7056ca9
CaioPQD <128498808+CaioPQD@users.noreply.github.com>
2023-06-23 00:07:03 -0300

    Update atividade2.cs

--- atividade2.cs
+++ atividade2.cs
@@ -11,13 +11,13 @@ namespace Lista1
             double cateto1 = 0;
             double cateto2 = 0;
         //apresentando erro de referencia nula, mas teoricamente a função de converter do teclado para a variavel esta certa 
-            Console.WriteLine("Digite um valor para o cateto 1: ");
+            Console.WriteLine("Informe um valor para o cateto 1: ");
             cateto1 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite um valor para cateto 2: ");
+            Console.WriteLine("Informe outro valor para cateto 2: ");
             cateto2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("A hipotenusa é: " + Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2)));
+            Console.WriteLine("A hipotenusa sera: " + Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2)));
         }
     }
 }
